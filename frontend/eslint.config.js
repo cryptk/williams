0cfc590ce9b5fdc5b91e3ae68e973f3154de8a93
Chris Jowett <421501+cryptk@users.noreply.github.com>
2025-10-09 21:59:34 -0500

    Disable eslint tailwind ordering, it disagrees with official prettier plugin

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,39 +1,44 @@
-import js from "@eslint/js";
-import globals from "globals";
-import preact from "eslint-config-preact";
-import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
-import tailwind from "eslint-plugin-tailwindcss";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import js from '@eslint/js'
+import globals from 'globals'
+import preact from 'eslint-config-preact'
+import css from '@eslint/css'
+import { defineConfig } from 'eslint/config'
+import tailwind from 'eslint-plugin-tailwindcss'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
 export default defineConfig([
-  ...tailwind.configs["flat/recommended"],
-  ...preact,
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
-  },
-  {
-    files: ["**/*.css"],
-    plugins: { css },
-    language: "css/css",
-    extends: ["css/recommended"],
-  },
-  {
-    languageOptions: {
-      globals: {
-        __APP_VERSION__: "readonly",
-      },
+    ...tailwind.configs['flat/recommended'],
+    ...preact,
+    {
+        files: ['**/*.{js,mjs,cjs,jsx}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: { globals: globals.browser },
     },
-  },
-  {
-    settings: {
-      tailwindcss: {
-        config: dirname(fileURLToPath(import.meta.url)) + "/src/styles/tailwind.css",
-      },
+    {
+        files: ['**/*.css'],
+        plugins: { css },
+        language: 'css/css',
+        extends: ['css/recommended'],
     },
-  },
-]);
+    {
+        languageOptions: {
+            globals: {
+                __APP_VERSION__: 'readonly',
+            },
+        },
+    },
+    {
+        settings: {
+            tailwindcss: {
+                config: dirname(fileURLToPath(import.meta.url)) + '/src/styles/tailwind.css',
+            },
+        },
+    },
+    {
+        rules: {
+            'tailwindcss/classnames-order': 'off', // handled by prettier plugin
+        },
+    },
+])
